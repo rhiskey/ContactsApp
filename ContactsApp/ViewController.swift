060aeba1cc3dd6a7dc3ad164a1930f10fb770053
Vladimir Kiselev <first3395@gmail.com>
2022-03-31 12:41:49 +0300

    Initial Commit

---
+++ ContactsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by Владимир Киселев on 31.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
